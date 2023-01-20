d4d92cf3ebca5becead3ff3850314cbec29e84ed
Alexander Samsonov <blendersamsonov@yandex.ru>
2023-01-20 14:33:09 +0500

    Configuration

--- usercfg.mk
+++ usercfg.mk
@@ -14,8 +14,8 @@
 
 # DRAFTON ?= # 1=on;0=off
 # SHOWMARKUP ?= # 1=on;0=off
-# FONTFAMILY ?= # 0=CMU;1=MS fonts;2=Liberation fonts
-# ALTFONT ?= # 0=Computer Modern;1=pscyr;2=XCharter
+# FONTFAMILY = 0 # 0=CMU;1=MS fonts;2=Liberation fonts
+# ALTFONT = 2 # 0=Computer Modern;1=pscyr;2=XCharter
 # USEBIBER ?= # 0=bibtex8;1=biber
 # USEFOOTCITE ?= # 0=no;1=yes
 # BIBGROUPED ?= # 0=no;1=yes
